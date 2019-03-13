c9ff9fd2b622b187ef6345c04150b74bfdeb03d8
Andrew <acarmstrong98@gmail.com>
2019-03-13 13:19:13 -0400

    added post and delete functionality

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ server.get('/api/users', (req, res) => {
     .then(users => {
         res.status(200).json(users)
     })
-    
+
     .catch(err => {
         res.status(500).json({ message: "The users information could not be retrieved." })
     }) 
@@ -29,9 +29,44 @@ server.get('/api/users/:id', (req, res) => {
             res.status(200).json(user)
         } else {
             res.status(404).json({ message: "The user with the specified ID does not exist." })
-        }        
+        } 
+
+    }).catch(err => {
+        res.status(500).json({ message:"The users information could not be retrieved." })
     })
 })
+
+server.post('/api/users', (req, res) => {
+    const user = req.body
+
+    database.insert(user)
+    .then(users => {
+
+        if (users) {
+            res.status(201).json(users)
+        } else {
+            res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+        }     
+    })
+    .catch(err => { 
+        res.status(500).json({ error: "There was an error while saving the user to the database" })
+    })
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id
+
+    database.remove(id)
+    .then(users => {
+        if (users) {
+            res.json(users)
+        } else {
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+        }
+    })
+    .catch(err => {
+        res.status(400).json({ errorMessage: "ERROR. This user could not be removed." })     
+    })
 })
 
 
